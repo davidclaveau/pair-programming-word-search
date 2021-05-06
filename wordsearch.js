f2fbf8c0c69ef8e402a02ed12c390fbcf28cceea
David Claveau <daveclaveau@gmail.com>
2021-05-06 00:46:54 +0000

    Changed functionality to wordsearch.js to properly fail if no word is found. Added functionality to search for words vertically. Added appropriate tests.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length < 1) {
+        return "Empty string given."
     }
+    
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    // console.log("horizontalJoin", horizontalJoin);
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    const verticalJoin = []
+    for (let i = 0; i < letters[0].length; i++) {
+        let vertWord = "";
+        for (let j = 0; j < letters.length; j++) {
+            vertWord += letters[j][i];
+        }
+        verticalJoin.push(vertWord);   
+    }
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;       
 }
 
 module.exports = wordSearch
